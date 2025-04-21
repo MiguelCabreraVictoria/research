d088b4f7f3b61d0d8c735f5d017d25c8a17c6a19
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-04-21 09:24:30 -0600

    LLM dataloader

---
+++ models/llm/dataloader/dataloader.py
@@ -0,0 +1,33 @@
+import torch
+import tiktoken
+
+from torch.utils.data import DataLoader
+from models.llm.dataloader.dataset import LLMDataset
+from utils.config_loader import LLM_CONFIG
+
+
+with open('/home/miguel/Desktop/research/data/the-verdict.txt', 'r') as file:
+    raw_text = file.read()
+
+
+tokenizer = tiktoken.get_encoding(LLM_CONFIG["dataset"]["encoder"])
+
+dataset = LLMDataset(raw_text,
+                     tokenizer, 
+                     LLM_CONFIG["dataset"])
+
+dataloader = DataLoader(dataset,
+                        batch_size=LLM_CONFIG["dataloader"]["batch_size"],
+                        shuffle=LLM_CONFIG["dataloader"]["shuffle"],
+                        drop_last=LLM_CONFIG["dataloader"]["drop_last"],
+                        num_workers=LLM_CONFIG["dataloader"]["num_workers"],
+                       )
+
+
+
+
+
+
+
+
+

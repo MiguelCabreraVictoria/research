e006d90e17a87906c650f5c09ce5945752aa76e0
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-04-21 21:05:58 -0600

    LLM evaluation

--- training/llm/evaluation.py
+++ training/llm/evaluation.py
@@ -1,27 +1,15 @@
 import os
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
+import torch
+from torch import nn
+from utils.config_loader import LLM_CONFIG
+from models.llm.model.gpt_model import GPTModel
 
-
-
-# Plot that shows the training loss and validation loss 
-
-def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
-    fig, ax1 = plt.subplots(figsize=(10, 5))
-    ax1.plot(epochs_seen, train_losses, label='Train Losses')
-    ax1.plot(epochs_seen, val_losses, linestyle="-." ,label='Validation Losses')
-    ax1.set_xlabel("Epochs")
-    ax1.set_ylabel("Loss")
-    ax1.legend(loc="upper right")
-    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax2 = ax1.twiny()
-    ax2.plot(tokens_seen, train_losses, alpha=0)
-    ax2.set_xlabel("Tokens seen")
-    fig.tight_layout()
-
-    path  = os.getcwd()
-    path = os.path.join(path, "images", "llm", "losses.png")
-    fig.savefig(path)
-    plt.close(fig)
-    print(f"Losses plot saved to {path}")
+checkpoint_path = os.path.join(os.getcwd(), "checkpoints", "model_and_optimizer.pt")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+checkpoint = torch.load(checkpoint_path, map_location=device)
+model = GPTModel(LLM_CONFIG['model'])
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer = torch.optim.AdamW(model.parameters(), lr=LLM_CONFIG['training']['learning_rate'], weight_decay=LLM_CONFIG['training']['weight_decay'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+model.train()
 

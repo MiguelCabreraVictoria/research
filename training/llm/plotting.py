23d4c2e13f7c76b447bc6cf43f75dddf2295f040
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-05-20 19:17:56 -0600

    plotting update

--- training/llm/plotting.py
+++ training/llm/plotting.py
@@ -12,6 +12,7 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     ax1.plot(epochs_seen, val_losses, linestyle="-." ,label='Validation Losses')
     ax1.set_xlabel("Epochs")
     ax1.set_ylabel("Loss")
+    ax1.set_ylim(0, 1.5)      
     ax1.legend(loc="upper right")
     ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax2 = ax1.twiny()

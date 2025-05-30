f2370e3b175803babcc91e3aae8920b05965b988
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-05-30 16:57:42 -0600

    update training v1.1

--- training/rl/training.py
+++ training/rl/training.py
@@ -1,14 +1,23 @@
 import os
 import torch
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from models.rl.agent import Agent
 from utils.config_loader import LLM_CONFIG
 
+
+# Se aplica una semilla para reproducibilidad
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
 loss_history = []
 avg_rewards = []
-avg_reward_bandit_1 = []
-avg_reward_bandit_2 = []
+avg_rewards_bandit_1 = []
+avg_rewards_bandit_2 = []
+
 
 
 
@@ -58,6 +67,8 @@ for epoch in range(num_epochs):
     total_reward = 0.0
     total_reward_bandit_1 = 0.0
     total_reward_bandit_2 = 0.0
+    n1 = len(prompts_bandit_1)
+    n2 = len(prompts_bandit_2)
     n_prompts = 0
 
     for prompt in prompts_bandit_1:
@@ -73,6 +84,7 @@ for epoch in range(num_epochs):
         loss = agent.policy_loss(log_prob, reward)
         total_loss += loss
         total_reward += reward
+        total_reward_bandit_1 += reward
         n_prompts += 1
 
     for prompt in prompts_bandit_2:
@@ -87,13 +99,17 @@ for epoch in range(num_epochs):
         loss = agent.policy_loss(log_prob, reward)
         total_loss += loss
         total_reward += reward
+        total_reward_bandit_2 += reward
         n_prompts += 1
 
+    total_prompts = n1 + n2
     total_loss /= n_prompts
-    avg_reward = total_reward / n_prompts
+    avg_reward = total_reward / total_prompts
 
     loss_history.append(total_loss.item())
     avg_rewards.append(avg_reward)
+    avg_rewards_bandit_1.append(total_reward_bandit_1 / n1)
+    avg_rewards_bandit_2.append(total_reward_bandit_2 / n2)
 
 
     total_loss.backward()
@@ -106,7 +122,24 @@ plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.title('Training Loss Epoch')
 plt.title('Training Loss Over Epochs - Policy Gradient')
+plt.grid()
 plt.savefig(os.path.join(os.getcwd(), "images","rl","training_loss.png"))
+
+plt.plot(avg_rewards, label="Average Reward (All Bandits)", color='purple')
+plt.xlabel("Epoch")
+plt.ylabel("Average Reward")
+plt.title("Overall Average Reward Over Epochs")
+plt.grid(True)
+plt.savefig(os.path.join("images", "rl", "avg_total_reward.png"))
+
+plt.plot(avg_rewards_bandit_1, label="Bandit 1 (bean vs chickpea)", color='green')
+plt.plot(avg_rewards_bandit_2, label="Bandit 2 (lentil vs rice)", color='orange')
+plt.xlabel("Epoch")
+plt.ylabel("Average Reward")
+plt.title("Average Reward per Bandit Over Epochs")
+plt.legend()
+plt.grid(True)
+plt.savefig(os.path.join("images", "rl", "reward_per_bandit.png"))
 
 # test_prompts = [
 #     "Which is better bean or lentil?",
